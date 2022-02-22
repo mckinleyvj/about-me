dcc61fb54104c00f244b79f1bb3142889beec0a6
MckinleyVJ <87624839+mckinleyvj@users.noreply.github.com>
2022-02-22 23:35:37 +1100

    updt: connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://0.0.0.0/fabulousme_db',
+  process.env.MONGODB_URI || 'mongodb://localhost/fabulousme_db',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
