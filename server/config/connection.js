280960803707016d367fef58c659a90390f1c4a8
MckinleyVJ <87624839+mckinleyvj@users.noreply.github.com>
2022-02-23 01:01:21 +1100

    updt: final fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/fabulousme_db?authSource=admin',
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fabulousme_db',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
