1e7cd06cabd7c06a87b140a9ba1cfe45b5ae4108
MckinleyVJ <87624839+mckinleyvj@users.noreply.github.com>
2022-02-26 23:57:07 +1100

    updt: added mediaquery for dashboard

--- client/src/components/Cards/Appointments.js
+++ client/src/components/Cards/Appointments.js
@@ -1,52 +1,42 @@
-import {
-    HStack,
-    Text,
-    useColorModeValue,
-    VStack,
-    Flex,
-    Badge,
-  } from "@chakra-ui/react";
-  import React from "react";
-  import { BiDotsVerticalRounded } from "react-icons/bi";
-  
-  const Appointments = ({ appointment }) => {
-    return (
-      <Flex
-        mx="auto"
-        bg={useColorModeValue("white", "gray.800")}
-        shadow="lg"
-        mb="2vh"
-        alignItems="center"
-        p={5}
-        justifyContent="space-between"
-        cursor="pointer" >
-        <Text
-          alignItems="center"
-          textAlign="left"
-          fontWeight="bold"
-          color="neutral.500" >
-          {appointment.datetime}
-        </Text>
-  
-        <Text color="neutral.500" fontWeight="bold">
-        {appointment.branch}
-              </Text> 
+import { HStack, Text, useColorModeValue, VStack, Flex, Badge } from '@chakra-ui/react';
+import React from 'react';
+import { BiDotsVerticalRounded } from 'react-icons/bi';
 
-              <Text color="neutral.500" fontWeight="bold">
-        {appointment.staff}
-        </Text>
-        <VStack alignItems="left">
-          <HStack>
-            <VStack alignItems="left" textAlign="left" spacing={0}>
-              <Badge colorScheme="green" py={3} px={10} borderRadius="30px">
-          {appointment.service_type}
-        </Badge>
-            </VStack>
-          </HStack>
-        </VStack>
-        <BiDotsVerticalRounded/>
-      </Flex>
-    );
-  };
-  
-  export default Appointments;
+const Appointments = ({ appointment }) => {
+	return (
+		<Flex
+			mx='auto'
+			bg={useColorModeValue('white', 'gray.800')}
+			shadow='lg'
+			mb='2vh'
+			alignItems='center'
+			p={5}
+			justifyContent='space-between'
+			cursor='pointer'
+			flexDirection='column'>
+			<Text alignItems='center' textAlign='left' fontWeight='bold' color='neutral.500'>
+				{appointment.datetime}
+			</Text>
+
+			<Text color='neutral.500' fontWeight='bold'>
+				{appointment.branch}
+			</Text>
+
+			<Text color='neutral.500' fontWeight='bold'>
+				{appointment.staff}
+			</Text>
+			<VStack alignItems='left'>
+				<HStack>
+					<VStack alignItems='left' textAlign='left' spacing={0}>
+						<Badge colorScheme='green' py={3} px={10} borderRadius='30px'>
+							{appointment.service_type}
+						</Badge>
+					</VStack>
+				</HStack>
+			</VStack>
+			{/* <BiDotsVerticalRounded /> */}
+		</Flex>
+	);
+};
+
+export default Appointments;
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,6 +1,15 @@
-import { Box, Center, Image, Flex, HStack, Stack, Text, VStack } from '@chakra-ui/react';
+import {
+	Box,
+	Center,
+	Image,
+	Flex,
+	HStack,
+	Stack,
+	Text,
+	VStack,
+	useMediaQuery,
+} from '@chakra-ui/react';
 import React from 'react';
-// import avatar from 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.clipartmax.com%2Fmiddle%2Fm2i8K9i8i8K9G6N4_blank-avatar-profile-pic-icon-female%2F&psig=AOvVaw3zrNfG_YUw1-l2VQhS-VzR&ust=1645531465291000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKjt8N_gkPYCFQAAAAAdAAAAABAD';
 import { GET_CUSTOMERINFO, GET_APPOINTMENTS } from '../utils/queries';
 import { useQuery } from '@apollo/client';
 import Header from '../components/Header/index';
@@ -9,6 +18,8 @@ import AppointmentsCard from '../components/Cards/Appointments';
 import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
+	const [isSmallerThan800] = useMediaQuery('(max-width: 800px)');
+
 	const navigate = useNavigate();
 	const isLoggedIn = Auth.loggedIn();
 
@@ -45,72 +56,135 @@ const Dashboard = () => {
 					src='https://images.unsplash.com/photo-1637777269308-6a072f24e8a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8'
 				/>
 			</Box>
+			<Header />
 			<Flex h='100vh' alignSelf='center' alignContent='center' flexDirection='column'>
-				<Header />
-
 				<Text fontSize='1.5em' textAlign='center' fontWeight='bold'>
 					My Dashboard
 				</Text>
-				<Flex w='80vw' alignSelf='center'>
-					<Stack w='70%' mr={5}>
-						<Flex
-							height='20vh'
-							alignItems='center'
-							borderRadius='5px'
-							bgGradient='linear(to-r,green.200 , brand.300)'
-							hover={{
-								color: 'white',
-								bg: 'brand.300',
-								ient: 'linear(to-r, brand.300, green.200 )',
-							}}
-							p={30}
-							sx={{ transition: 'color 0.5s ease 0s' }}>
-							<VStack alignItems='left' ml={30}>
-								<HStack>
-									<Text fontSize='1.3rem' textAlign='left'>
-										Hi,
-									</Text>{' '}
-									<Text fontWeight='bold' fontSize='1.3rem'>
-										{cust_info.firstname} {cust_info.lastname}
-									</Text>
-								</HStack>
-								<Text textAlign='left'>
-									Lets see what are your scheduled appointments.
-								</Text>
-							</VStack>
-						</Flex>
-
-						<Text textAlign='left' fontSize='1.2em' p={3} fontWeight='bold'>
-							Scheduled Appointments
-						</Text>
-
-						<Box width='100%' bg='white' borderRadius='5px' height='60%' mt='30px'>
-							{appt_info.map((appt) => {
-								return <AppointmentsCard appointment={appt} key={appt._id} />;
-							})}
-						</Box>
-					</Stack>
-					<Stack w='30%' height='80vh'>
-						<Flex
-							onClick={handleAppt}
-							width='100%'
-							bg='white'
-							borderRadius='5px'
-							h='70px'
-							alignItems='center'
-							_hover={{ bg: 'brand.200' }}
-							cursor='pointer'>
-							<Center
-								ml={5}
-								w='30px'
-								h='30px'
+				{isSmallerThan800 ? (
+					<Flex w='95vw' flexDirection='column' alignSelf='center'>
+						<Stack w='100%' mr={5}>
+							<Flex
+								height='20vh'
+								alignItems='center'
+								borderRadius='5px'
 								bgGradient='linear(to-r,green.200 , brand.300)'
-								color='white'
-								borderRadius='5px'></Center>
-							<Text ml={3}>Make an appointment</Text>
-						</Flex>
-					</Stack>
-				</Flex>
+								hover={{
+									color: 'white',
+									bg: 'brand.300',
+									ient: 'linear(to-r, brand.300, green.200 )',
+								}}
+								// p={30}
+								sx={{ transition: 'color 0.5s ease 0s' }}>
+								<VStack alignItems='left' mx='15'>
+									<HStack>
+										<Text fontSize='1.3rem' textAlign='left'>
+											Hi,
+										</Text>{' '}
+										<Text fontWeight='bold' fontSize='1.3rem'>
+											{cust_info.firstname} {cust_info.lastname}
+										</Text>
+									</HStack>
+									<Text textAlign='left'>
+										Lets see what are your scheduled appointments.
+									</Text>
+								</VStack>
+							</Flex>
+
+							<Text textAlign='left' fontSize='1.2em' p={3} fontWeight='bold'>
+								Scheduled Appointments
+							</Text>
+
+							<Box width='100%' bg='white' borderRadius='5px' height='60%' mt='30px'>
+								{appt_info.map((appt) => {
+									return <AppointmentsCard appointment={appt} key={appt._id} />;
+								})}
+							</Box>
+						</Stack>
+						<Stack w='100%' height='80vh'>
+							<Flex
+								onClick={handleAppt}
+								width='100%'
+								bg='white'
+								borderRadius='5px'
+								h='70px'
+								alignItems='center'
+								_hover={{ bg: 'brand.200' }}
+								cursor='pointer'
+								justifyContent='center'>
+								<Center
+									ml={5}
+									w='30px'
+									h='30px'
+									bgGradient='linear(to-r,green.200 , brand.300)'
+									color='white'
+									borderRadius='5px'></Center>
+								<Text ml={3}>Make an appointment</Text>
+							</Flex>
+						</Stack>
+					</Flex>
+				) : (
+					<Flex w='80vw' alignSelf='center'>
+						<Stack w='70%' mr={5}>
+							<Flex
+								height='20vh'
+								alignItems='center'
+								borderRadius='5px'
+								bgGradient='linear(to-r,green.200 , brand.300)'
+								hover={{
+									color: 'white',
+									bg: 'brand.300',
+									ient: 'linear(to-r, brand.300, green.200 )',
+								}}
+								p={30}
+								sx={{ transition: 'color 0.5s ease 0s' }}>
+								<VStack alignItems='left' ml={30}>
+									<HStack>
+										<Text fontSize='1.3rem' textAlign='left'>
+											Hi,
+										</Text>{' '}
+										<Text fontWeight='bold' fontSize='1.3rem'>
+											{cust_info.firstname} {cust_info.lastname}
+										</Text>
+									</HStack>
+									<Text textAlign='left'>
+										Lets see what are your scheduled appointments.
+									</Text>
+								</VStack>
+							</Flex>
+
+							<Text textAlign='left' fontSize='1.2em' p={3} fontWeight='bold'>
+								Scheduled Appointments
+							</Text>
+
+							<Box width='100%' bg='white' borderRadius='5px' height='60%' mt='30px'>
+								{appt_info.map((appt) => {
+									return <AppointmentsCard appointment={appt} key={appt._id} />;
+								})}
+							</Box>
+						</Stack>
+						<Stack w='30%' height='80vh'>
+							<Flex
+								onClick={handleAppt}
+								width='100%'
+								bg='white'
+								borderRadius='5px'
+								h='70px'
+								alignItems='center'
+								_hover={{ bg: 'brand.200' }}
+								cursor='pointer'>
+								<Center
+									ml={5}
+									w='30px'
+									h='30px'
+									bgGradient='linear(to-r,green.200 , brand.300)'
+									color='white'
+									borderRadius='5px'></Center>
+								<Text ml={3}>Make an appointment</Text>
+							</Flex>
+						</Stack>
+					</Flex>
+				)}
 			</Flex>
 		</Box>
 	);
