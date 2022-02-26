1055bdc0c634f4ca55178cd36f0ed88edc6aedb8
MckinleyVJ <87624839+mckinleyvj@users.noreply.github.com>
2022-02-26 22:53:14 +1100

    updt: added mediaquery for login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -11,12 +11,15 @@ import {
 	Text,
 	Flex,
 	Image,
+	useMediaQuery,
 } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 import Auth from '../utils/auth';
 import { LOGIN_USER } from '../utils/mutations';
 
 const Login = () => {
+	const [isSmallerThan800] = useMediaQuery('(max-width: 800px)');
+
 	const navigate = useNavigate();
 	const isLoggedIn = Auth.loggedIn();
 
@@ -53,85 +56,185 @@ const Login = () => {
 	};
 
 	return (
-		<Box overflowX='hidden'>
-			<Box w='100%' h='100%' opacity='0.1' pos='fixed' zIndex='-9999'>
-				<Image
-					h='100vh'
-					w='100vw'
-					src='https://images.unsplash.com/photo-1644916081706-e98b1fadc319?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8'
-				/>
-			</Box>
-			<Flex flexDirection='column'>
-				<HStack mt='3vh' ml='57%'>
-					<Button color='#555555' variant='link' onClick={() => navigate(`/signup`)}>
-						<Text>Don't have an account? </Text>
-						<Text ml='0.3vw' color='brand.300' fontWeight='500'>
-							Sign up
-						</Text>
-					</Button>
-				</HStack>
-				<Flex w='30vw' alignSelf='center' flexDirection='column' justifyContent='center'>
-					<Text align='left' fontSize='2rem'>
-						Login to my account
-					</Text>
-					<Text align='left' color='brand.500' fontSize='0.8rem'>
-						Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-					</Text>
-					<form id='login' onSubmit={handleSubmit}>
-						<FormControl mt='2vh' isRequired>
-							<FormLabel>Username/Email</FormLabel>
-							<Input
-								id='email'
-								name='emailVal'
-								type='text'
-								placeholder='Enter your username/email'
-								value={userFormInput.emailVal}
-								onChange={handleInputChange}
-							/>
-						</FormControl>
-						<FormControl mt='3vh' isRequired>
-							<FormLabel>Password</FormLabel>
-							<InputGroup>
-								<Input
-									id='password'
-									name='passwordVal'
-									type='password'
-									placeholder='Enter your password'
-									value={userFormInput.passwordVal}
-									onChange={handleInputChange}
-								/>
-							</InputGroup>
-						</FormControl>
-						{error === true ? (
-							<Box mt='5'>
-								<Flex color='red'>Error: </Flex>
-								<Flex>
-									Could not log you in. Please check username and password.
-								</Flex>
-							</Box>
-						) : (
-							''
-						)}
-						<Button
-							mt='3vh'
-							mb='2vh'
-							type='submit'
-							width='100%'
-							_focus={{ outline: 'none' }}
-							_active={{ outline: 'none' }}>
-							Login to my account
-						</Button>
-						<Button
-							width='100%'
-							_focus={{ outline: 'none' }}
-							_active={{ outline: 'none' }}
-							onClick={() => navigate(`/`)}>
-							Back to Home Page
-						</Button>
-					</form>
+		<>
+			{isSmallerThan800 ? (
+				<Flex overflowX='hidden' h='100vh' justifyContent='center' alignItems='center'>
+					<Box w='100%' h='100%' opacity='0.1' pos='fixed' zIndex='-9999'>
+						<Image
+							h='100vh'
+							w='100vw'
+							src='https://images.unsplash.com/photo-1644916081706-e98b1fadc319?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8'
+						/>
+					</Box>
+					<Flex flexDirection='column'>
+						<HStack mb='3vh' justifyContent='center'>
+							<Button
+								color='#555555'
+								variant='link'
+								onClick={() => navigate(`/signup`)}>
+								<Text>Don't have an account? </Text>
+								<Text ml='0.3vw' color='brand.300' fontWeight='500'>
+									Sign up
+								</Text>
+							</Button>
+						</HStack>
+						<Flex
+							w='80vw'
+							alignSelf='center'
+							flexDirection='column'
+							justifyContent='center'>
+							<Text align='left' fontSize='2rem'>
+								Login to my account
+							</Text>
+							<Text align='left' color='brand.500' fontSize='0.8rem'>
+								The email you've used during registration is your username.
+							</Text>
+							<form id='login' onSubmit={handleSubmit}>
+								<FormControl mt='2vh' isRequired>
+									<FormLabel>Username/Email</FormLabel>
+									<Input
+										id='email'
+										name='emailVal'
+										type='text'
+										placeholder='Enter your username/email'
+										value={userFormInput.emailVal}
+										onChange={handleInputChange}
+									/>
+								</FormControl>
+								<FormControl mt='3vh' isRequired>
+									<FormLabel>Password</FormLabel>
+									<InputGroup>
+										<Input
+											id='password'
+											name='passwordVal'
+											type='password'
+											placeholder='Enter your password'
+											value={userFormInput.passwordVal}
+											onChange={handleInputChange}
+										/>
+									</InputGroup>
+								</FormControl>
+								{error === true ? (
+									<Box mt='5'>
+										<Flex color='red'>Error: </Flex>
+										<Flex>
+											Could not log you in. Please check username and
+											password.
+										</Flex>
+									</Box>
+								) : (
+									''
+								)}
+								<Button
+									mt='3vh'
+									mb='2vh'
+									type='submit'
+									width='100%'
+									_focus={{ outline: 'none' }}
+									_active={{ outline: 'none' }}>
+									Login to my account
+								</Button>
+								<Button
+									width='100%'
+									_focus={{ outline: 'none' }}
+									_active={{ outline: 'none' }}
+									onClick={() => navigate(`/`)}>
+									Back to Home Page
+								</Button>
+							</form>
+						</Flex>
+					</Flex>
 				</Flex>
-			</Flex>
-		</Box>
+			) : (
+				<Box overflowX='hidden'>
+					<Box w='100%' h='100%' opacity='0.1' pos='fixed' zIndex='-9999'>
+						<Image
+							h='100vh'
+							w='100vw'
+							src='https://images.unsplash.com/photo-1644916081706-e98b1fadc319?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8'
+						/>
+					</Box>
+					<Flex flexDirection='column'>
+						<HStack mt='3vh' ml='57%'>
+							<Button
+								color='#555555'
+								variant='link'
+								onClick={() => navigate(`/signup`)}>
+								<Text>Don't have an account? </Text>
+								<Text ml='0.3vw' color='brand.300' fontWeight='500'>
+									Sign up
+								</Text>
+							</Button>
+						</HStack>
+						<Flex
+							w='30vw'
+							alignSelf='center'
+							flexDirection='column'
+							justifyContent='center'>
+							<Text align='left' fontSize='2rem'>
+								Login to my account
+							</Text>
+							<Text align='left' color='brand.500' fontSize='0.8rem'>
+								The email you've used during registration is your username.
+							</Text>
+							<form id='login' onSubmit={handleSubmit}>
+								<FormControl mt='2vh' isRequired>
+									<FormLabel>Username/Email</FormLabel>
+									<Input
+										id='email'
+										name='emailVal'
+										type='text'
+										placeholder='Enter your username/email'
+										value={userFormInput.emailVal}
+										onChange={handleInputChange}
+									/>
+								</FormControl>
+								<FormControl mt='3vh' isRequired>
+									<FormLabel>Password</FormLabel>
+									<InputGroup>
+										<Input
+											id='password'
+											name='passwordVal'
+											type='password'
+											placeholder='Enter your password'
+											value={userFormInput.passwordVal}
+											onChange={handleInputChange}
+										/>
+									</InputGroup>
+								</FormControl>
+								{error === true ? (
+									<Box mt='5'>
+										<Flex color='red'>Error: </Flex>
+										<Flex>
+											Could not log you in. Please check username and
+											password.
+										</Flex>
+									</Box>
+								) : (
+									''
+								)}
+								<Button
+									mt='3vh'
+									mb='2vh'
+									type='submit'
+									width='100%'
+									_focus={{ outline: 'none' }}
+									_active={{ outline: 'none' }}>
+									Login to my account
+								</Button>
+								<Button
+									width='100%'
+									_focus={{ outline: 'none' }}
+									_active={{ outline: 'none' }}
+									onClick={() => navigate(`/`)}>
+									Back to Home Page
+								</Button>
+							</form>
+						</Flex>
+					</Flex>
+				</Box>
+			)}
+		</>
 	);
 };
 
